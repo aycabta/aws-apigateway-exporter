e1902913c95e318346b7b0baf335831395a83c42
Code Ass <aycabta@gmail.com>
2015-11-18 21:59:20 +0900

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var rename = require('gulp-rename');
+
+gulp.task('build', ['build-bin', 'build-lib']);
+
+gulp.task('build-bin', function() {
+    gulp.src('src/index.js')
+        .pipe(babel())
+        .pipe(rename('aws-apigateway-exporter'))
+        .pipe(gulp.dest('bin'));
+});
+
+gulp.task('build-lib', function() {
+    gulp.src('src/get_parameters.js')
+        .pipe(babel())
+        .pipe(gulp.dest('lib'));
+});
+
+gulp.task('run', ['build'], function() {
+    require('child_process').exec('node bin/aws-apigateway-exporter', function (error, stdout, stderr) {
+        console.log(stdout);
+        console.error(stderr);
+    });
+});
+
